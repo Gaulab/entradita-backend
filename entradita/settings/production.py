e020e384e14a8a92d638caf87ef82d22f7dd3d6f
aguilarivan <ivanaguilarnardin@gmail.com>
2024-12-24 17:09:38 -0300

    bdd bien

--- entradita/settings/production.py
+++ entradita/settings/production.py
@@ -14,17 +14,17 @@ ALLOWED_HOSTS = ['*']
 # Base de datos de producción (URL en variable de entorno)
 
 # Configuración de la base de datos SQLite para producción
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default=os.getenv('DATABASE_URL')
-#     )
-# }     
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
+}     
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 # Orígenes de CORS específicos para producción
 CORS_ALLOW_ALL_ORIGINS = True
 
