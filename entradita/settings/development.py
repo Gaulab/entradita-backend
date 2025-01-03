3223d0f078e8d0f0690b8a98b68e79882ec02ff1
aguilarivan <ivanaguilarnardin@gmail.com>
2025-01-03 00:20:47 -0300

    new

--- entradita/settings/development.py
+++ entradita/settings/development.py
@@ -11,17 +11,17 @@ DEBUG = True
 # Hosts permitidos en desarrollo
 ALLOWED_HOSTS = ['*']
 # Base de datos local
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default=os.getenv('DATABASE_URL')
-#     )
-# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 # Orígenes CORS permitidos para desarrollo
 CORS_ALLOW_ALL_ORIGINS = True
 
