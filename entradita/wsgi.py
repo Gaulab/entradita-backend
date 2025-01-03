f442865bb0223fec4a669db9a2e831822336eff8
aguilarivan <ivanaguilarnardin@gmail.com>
2025-01-03 17:11:10 -0300

    news

--- entradita/wsgi.py
+++ entradita/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'entradita.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'entradita.settings.production')
 
 application = get_wsgi_application()
