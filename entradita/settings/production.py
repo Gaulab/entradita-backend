0c9a2d3f00f8f58794c492cb0abac1d54a700397
aguilarivan <ivanaguilarnardin@gmail.com>
2025-01-07 20:55:12 -0300

    code sorted

--- entradita/settings/production.py
+++ entradita/settings/production.py
@@ -10,7 +10,8 @@ load_dotenv()
 DEBUG = True
 
 # Hosts permitidos en producción
-ALLOWED_HOSTS = ['entraditaback-production.up.railway.app']
+#ALLOWED_HOSTS = ['entraditaback-production.up.railway.app']
+ALLOWED_HOST = ['entraditabackend.up.railway.app']
 
 # Configuración de la base de datos SQLite para producción
 DATABASES = {
@@ -21,6 +22,8 @@ DATABASES = {
 
 # Orígenes de CORS específicos para producción
 CORS_ALLOWED_ORIGINS = [
+    'https://entraditafrontend-production.up.railway.app',
+    'https://www.entraditafrontend-production.up.railway.app',
     'https://www.entradita.com',
     'https://www.entradita.net',
     'https://www.entradita.app',
@@ -29,6 +32,8 @@ CORS_ALLOWED_ORIGINS = [
 
 # CSRF Trusted Origins
 CSRF_TRUSTED_ORIGINS = [
+        'https://entraditafrontend-production.up.railway.app',
+    'https://www.entraditafrontend-production.up.railway.app',
     'https://www.entradita.com',
     'https://www.entradita.net',
     'https://www.entradita.app',
